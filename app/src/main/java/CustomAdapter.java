2ac96d841222ff874510ff7555ffce0931035b10
Kasper Sørensen <ksare17@student.aau.dk>
2018-04-24 10:45:33 +0200

    Har oprettet en Custom Adapter, der ikke fungerer

--- app/src/main/java/CustomAdapter.java
+++ app/src/main/java/CustomAdapter.java
@@ -13,8 +13,8 @@ public class CustomAdapter extends ArrayAdapter<Item> {
     public CustomAdapter(@NonNull Context context, int resource) {
         super(context, resource);
 
-        private static class ArrayAdapter<List> Item;
-                Context mContex00t;
+         ArrayAdapter itemOfArrays[10] = new Item;
+                Context mContext;
 
         class ViewHolder {
         TextView txtName;
@@ -23,7 +23,7 @@ public class CustomAdapter extends ArrayAdapter<Item> {
         TextView storageMethod;
         TextView openClosed;
 
-        public CustomAdapter(ArrayAdapter<Item> data, Context context) {
+        public ViewHolder(ArrayAdapter<Item> data, Context context) {
             super(context, activity_item, data);
             this.Item = data;
             this.mContext = context;
