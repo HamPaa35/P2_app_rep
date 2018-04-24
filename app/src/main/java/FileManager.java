e17846d337b7c9bb4276616c9d7dc487ee2430a2
HamPaa35 <mfaldt17@student.aau.dk>
2018-04-24 11:26:44 +0200

    Added new csv reader module

--- app/src/main/java/FileManager.java
+++ app/src/main/java/FileManager.java
@@ -1,3 +1,30 @@
-public class FileManager {
+import android.os.Environment;
 
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+
+public class FileManager {
+    static void writeToFile(){
+        String baseDir = Environment.getExternalStorageDirectory().getAbsolutePath();
+        String fileName = "AnalysisData.csv";
+        String filePath = baseDir + File.separator + fileName;
+        File f = new File(filePath );
+        CSVWriter writer;
+// File exist
+        if(f.exists() && !f.isDirectory()){
+            FileWriter mFileWriter = new FileWriter(filePath , true);
+            writer = new CSVWriter(mFileWriter);
+        }
+        else {
+            writer = new CSVWriter(new FileWriter(filePath));
+        }
+        String[] data = {"Ship Name","Scientist Name", "...",new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").formatter.format(date)});
+
+        writer.writeNext(data);
+
+        writer.close();
+    }
 }
